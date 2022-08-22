e7fe21da849ab592df24a60458da3caea47bf8c5
Konrad Kubczyk <konrad@kubczyk.com>
2022-08-23 00:51:43 +0200

    Add navbar

--- routes/sites.js
+++ routes/sites.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Auth = require('../utils/auth');
 
 router.get('/', Auth.isAuthorized(), (req, res, next) => {
-    res.render('sites');
+    res.render('sites', {title: 'Your sites', userEmail: req.user.userEmail});
 });
 
 module.exports = router;
