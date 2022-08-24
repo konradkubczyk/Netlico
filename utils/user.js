607f3281c7ec6b34a676c465998d574b4a8ff250
Konrad Kubczyk <konrad@kubczyk.com>
2022-08-24 04:56:12 +0200

    Add user data getters and hide properties

--- utils/user.js
+++ utils/user.js
@@ -4,6 +4,13 @@ const jwt = require("jsonwebtoken");
 const UserData = require("../schema/user");
 
 class User {
+    #id;
+    #email;
+    #emailVerified;
+    #hashedPassword;
+    #websites;
+    #isAdmin;
+
     static #jwtSecret = process.env.JWT_SECRET;
 
     static async register(email, plainPassword) {
@@ -80,16 +87,16 @@ class User {
     }
 
     constructor(userId) {
-        this.id = userId;
+        this.#id = userId;
     }
 
     async loadUserData() {
         const userData = await UserData.findById(this.id);
-        this.email = userData.email;
-        this.emailVerified = userData.emailVerified;
-        this.hashedPassword = userData.hashedPassword;
-        this.websites = userData.websites;
-        this.isAdmin = userData.isAdmin;
+        this.#email = userData.email;
+        this.#emailVerified = userData.emailVerified;
+        this.#hashedPassword = userData.hashedPassword;
+        this.#websites = userData.websites;
+        this.#isAdmin = userData.isAdmin;
     }
     
     async delete() {
@@ -105,6 +112,66 @@ class User {
                 message: 'Could not delete user data'
             }
         }
+    }
+
+    get id() {
+        if (typeof this.#id !== 'undefined') {
+            return this.#id;
+        }
+        return (async () => {
+            await this.loadUserData();
+            return this.#id;
+        })();
+    }
+
+    get email() {
+        if (typeof this.#email !== 'undefined') {
+            return this.#email;
+        }
+        return (async () => {
+            await this.loadUserData();
+            return this.#email;
+        })();
+    }
+
+    get hashedPassword() {
+        if (typeof this.#hashedPassword !== 'undefined') {
+            return this.#hashedPassword;
+        }
+        return (async () => {
+            await this.loadUserData();
+            return this.#hashedPassword;
+        })();
+    }
+
+    get emailVerified() {
+        if (typeof this.#emailVerified !== 'undefined') {
+            return this.#emailVerified;
+        }
+        return (async () => {
+            await this.loadUserData();
+            return this.#emailVerified;
+        })();
+    }
+
+    get websites() {
+        if (typeof this.#websites !== 'undefined') {
+            return this.#websites;
+        }
+        return (async () => {
+            await this.loadUserData();
+            return this.#websites;
+        })();
+    }
+
+    get isAdmin() {
+        if (typeof this.#isAdmin !== 'undefined') {
+            return this.#isAdmin;
+        }
+        return (async () => {
+            await this.loadUserData();
+            return this.#isAdmin;
+        })();
     }
 }
 
