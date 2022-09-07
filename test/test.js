e809ef13c3cfa1fb6c442857bd8ea0bf4fc0ff7a
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-07 11:08:08 +0200

    Update test suite

--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
+require('../bin/www');
+
 const request = require('supertest');
 const app = require('../app');
 
-describe('App', function() {
-  it('has the default page', function(done) {
+describe('App', function () {
+  it('has the homepage', function (done) {
     request(app)
       .get('/')
-      .expect(/Welcome to Express/, done);
+      .expect(/Get started/, done);
   });
-}); 
+  it('redirects unauthorized clients', function (done) {
+    request(app)
+      .get('/account')
+      .expect(302, done);
+  });
+});
