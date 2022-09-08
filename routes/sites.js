6403f48f4ab199a47b9d257e463945649b09c0f4
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-08 23:40:43 +0200

    Add site creation and deletion

--- routes/sites.js
+++ routes/sites.js
@@ -2,9 +2,31 @@ const express = require('express');
 const router = express.Router();
 
 const Auth = require('../utils/auth');
+const User = require('../models/user');
+const Site = require('../models/site');
 
-router.get('/', Auth.isAuthorized(), (req, res, next) => {
-    res.render('sites', { title: 'Your sites', userEmail: req.user.email, currentUrl: req.originalUrl });
+router.get('/', Auth.isAuthorized(), async (req, res, next) => {
+    const user = new User(req.user.id);
+    await user.read();
+    const sites = [];
+    for (const siteId of user.sites) {
+        const site = new Site(siteId);
+        await site.read();
+        sites.push(site);
+    }
+    res.render('sites', { title: 'Your sites', userEmail: req.user.email, currentUrl: req.originalUrl, sites });
+});
+
+router.post('/create', Auth.isAuthorized(), async (req, res, next) => {
+    const user = new User(req.user.id);
+    const newSiteId = await user.createSite();
+    res.status(200).json({ siteId: newSiteId });
+});
+
+router.delete('/:siteId/delete', Auth.isAuthorized(), async (req, res, next) => {
+    const user = new User(req.user.id);
+    await user.deleteSite(req.params.siteId);
+    res.status(200).json({ success: true });
 });
 
 module.exports = router;
