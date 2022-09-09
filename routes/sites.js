aa88b47bc34ffb081be61eb7e45284e66eb49019
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-09 20:14:59 +0200

    Implement data autosave and preview resfresh

--- routes/sites.js
+++ routes/sites.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+const path = require('path');
+
 const Auth = require('../utils/auth');
 const User = require('../models/user');
 const Site = require('../models/site');
+const Page = require('../models/page');
 
 router.get('/', Auth.isAuthorized(), async (req, res, next) => {
     const user = new User(req.user.id);
@@ -29,7 +33,39 @@ router.get('/:siteId', Auth.isAuthorized(), async (req, res, next) => {
     if (user.sites.includes(req.params.siteId)) {
         const site = new Site(req.params.siteId);
         await site.read();
-        res.render('editor', { title: site.title, userEmail: req.user.email, currentUrl: req.originalUrl, site });
+
+        // Read list of themes data
+        const themesDataPath = path.join(__dirname, '../config/themes.json');
+        const themes = JSON.parse(fs.readFileSync(themesDataPath));
+
+        res.render('editor', { title: site.title, userEmail: req.user.email, currentUrl: req.originalUrl, site, themes });
+    } else {
+        res.status(401).render('error', { errorCode: 401, errorMessage: 'Unauthorized' });
+    }
+});
+
+router.get('/:siteId/preview', Auth.isAuthorized(), async (req, res, next) => {
+    res.redirect(`/sites/${req.params.siteId}/0/preview`);
+});
+
+router.get('/:siteId/:pageNumber/preview', Auth.isAuthorized(), async (req, res, next) => {
+    const user = new User(req.user.id);
+    await user.read();
+
+    // Check if the user owns the site
+    if (user.sites.includes(req.params.siteId)) {
+        const site = new Site(req.params.siteId);
+        await site.read();
+
+        // Reads pages of the site
+        const pages = [];
+        for (const pageNumber of site.pages) {
+            const page = new Page(pageNumber);
+            await page.read();
+            pages.push(page);
+        }
+        pages.sort((a, b) => a.position - b.position);
+        res.render(`themes/${site.theme}`, { site, page: pages[req.params.pageNumber] });
     } else {
         res.status(401).render('error', { errorCode: 401, errorMessage: 'Unauthorized' });
     }
@@ -45,6 +81,35 @@ router.delete('/:siteId/:pageId/delete', Auth.isAuthorized(), async (req, res, n
     const user = new User(req.user.id);
     await user.deletePage(req.params.siteId, req.params.pageId);
     res.status(200).json({ success: true });
+});
+
+router.patch('/:siteId/update', Auth.isAuthorized(), async (req, res, next) => {
+    const user = new User(req.user.id);
+    await user.read();
+    if (user.sites.includes(req.params.siteId)) {
+        switch (req.body.entityType) {
+            case 'site':
+                const site = new Site(req.params.siteId);
+                await site.read();
+                await site.updateProperty(req.body.property, req.body.value);
+                res.status(200).json({ success: true });
+                break;
+            case 'page':
+                const page = new Page(req.body.entityId);
+                await page.read();
+                if (page.siteId === req.params.siteId) {
+                    await page.updateProperty(req.body.property, req.body.value);
+                    res.status(200).json({ success: true });
+                } else {
+                    res.status(401).json({ success: false });
+                }
+                break;
+            default:
+                res.status(400).json({ success: false });
+        }
+    } else {
+        res.status(401).json({ success: false });
+    }
 });
 
 module.exports = router;
