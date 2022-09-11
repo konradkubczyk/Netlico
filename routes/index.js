7ec8e2609f5cfb3f6e8264cd36a41a86181b3992
Konrad Kubczyk <konrad@kubczyk.com>
2022-09-11 15:21:29 +0200

    Index route simplification and bug fix

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,17 @@ const router = express.Router();
 
 const Auth = require('../utils/auth');
 const Site = require('../models/site');
-const Page = require('../models/page');
 
-router.get('/:path', async (req, res, next) => {
+router.get('/:path?', async (req, res, next) => {
     if (req.subdomains.length) {
         const site = await Site.findBySubdomain(req.subdomains[0]);
-        site.render(res, null, req.params.path);
-    } else {
-        next();
-    }
-});
-
-router.get('/', async (req, res, next) => {
-    if (req.subdomains.length) {
-        const site = await Site.findBySubdomain(req.subdomains[0]);
-        site.render(res, null, req.params.path);
+        if (site) {
+            site.render(res, null, req.params.path);
+        } else {
+            // Get main domain without the subdomain
+            const domain = req.get('host').split('.').slice(1).join('.');
+            res.redirect(`https://${domain}`);
+        }
     } else {
         const isLoggedIn = Auth.isLoggedIn(req.cookies.authToken);
         res.render('index', { isLoggedIn: isLoggedIn });
